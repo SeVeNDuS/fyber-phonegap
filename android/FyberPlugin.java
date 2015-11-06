ecfe5aa73ca35cce88e6e3d9b5f832a0cc1aefcc
SeVeNDuS <sevendus@gmail.com>
2015-11-07 00:01:56 +0100

    Migration to fyber-sdk-8.0.1 - unfinished

--- android/FyberPlugin.java
+++ android/FyberPlugin.java
@@ -6,8 +6,22 @@ import java.util.Map;
 import android.content.Intent;
 import android.util.Log;
 
-import com.sponsorpay.SponsorPay;
-import com.sponsorpay.publisher.SponsorPayPublisher;
+import com.fyber.Fyber;
+import com.fyber.utils.FyberLogger;
+import com.fyber.requesters.OfferWallRequester;
+
+import com.fyber.ads.videos.RewardedVideoActivity;
+import com.fyber.currency.VirtualCurrencyErrorResponse;
+import com.fyber.currency.VirtualCurrencyResponse;
+import com.fyber.requesters.RewardedVideoRequester;
+import com.fyber.requesters.VirtualCurrencyCallback;
+import com.fyber.requesters.VirtualCurrencyRequester;
+
+import com.fyber.requesters.InterstitialRequester;
+
+import com.fyber.ads.AdFormat;
+import com.fyber.requesters.RequestCallback;
+import com.fyber.requesters.RequestError;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -17,16 +31,19 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class FyberPlugin extends CordovaPlugin {
+public class FyberPlugin extends CordovaPlugin implements RequestCallback, VirtualCurrencyCallback{
 
     private static final String LOGTAG = "FyberPlugin";
     private static final String DEFAULT_APP_KEY = "27434";
 
-    private static final int OFFERWALL_REQUEST_CODE = 12102013;
+    protected static final int INTERSTITIAL_REQUEST_CODE = 8792;
+    protected static final int OFFERWALL_REQUEST_CODE = 8795;
+    protected static final int REWARDED_VIDEO_REQUEST_CODE = 8796;
 
     private static final String ACTION_INITIALIZE = "initialize";
     private static final String ACTION_SHOW_OFFERWALL = "showOfferwall";
     private static final String ACTION_SHOW_REWARDEDVIDEO = "showRewardedVideo";
+    private static final String ACTION_SHOW_INTERSTITIAL = "showInterstitial";
     private static final String OPT_APPLICATION_KEY = "appKey";
     private static final String OPT_USER_ID = "userId";
     private static final String OPT_SECURITY_TOKEN = "securityToken";
@@ -48,6 +65,9 @@ public class FyberPlugin extends CordovaPlugin {
         } else if (ACTION_SHOW_REWARDEDVIDEO.equals(action)) {
             JSONObject options = args.optJSONObject(0);
             result = executeShowRewardedVideo(options, callbackContext);
+        } else if (ACTION_SHOW_INTERSTITIAL.equals(action)) {
+            JSONObject options = args.optJSONObject(0);
+            result = executeShowInterstitial(options, callbackContext);
         }
 
         if (result != null) callbackContext.sendPluginResult( result );
@@ -71,9 +91,20 @@ public class FyberPlugin extends CordovaPlugin {
         if(options.has(OPT_SECURITY_TOKEN)) this.securityToken = options.optString( OPT_SECURITY_TOKEN );
 
         try {
-            SponsorPay.start(this.appKey, this.userId, this.securityToken, cordova.getActivity());
-        } catch (RuntimeException e){
-            Log.d(LOGTAG, e.getLocalizedMessage());
+
+            // ** SDK INITIALIZATION **
+            //when you start Fyber SDK you get a Settings object that you can use to customise the SDK behaviour.
+            //Have a look at the method 'customiseFyberSettings' to learn more about possible customisation.
+            Fyber.Settings fyberSettings = Fyber
+                    .with(this.appKey, , cordova.getActivity())
+                    .withSecurityToken(this.securityToken)
+// by default Fyber SDK will start precaching. If you wish to only start precaching at a later time you can uncomment this line and use 'CacheManager' to start, pause or resume on demand.
+//                  .withManualPrecaching()
+// if you do not provide an user id Fyber SDK will generate one for you
+                    .withUserId(this.userId)
+                    .start();
+        } catch (IllegalArgumentException e) {
+            Log.d(TAG, e.getLocalizedMessage());
         }
     }
 
@@ -88,8 +119,9 @@ public class FyberPlugin extends CordovaPlugin {
     }
 
     private void showOfferWall(JSONObject options) {
-        Intent offerWallIntent = SponsorPayPublisher.getIntentForOfferWallActivity(cordova.getActivity().getApplicationContext(), true);
-        cordova.getActivity().startActivityForResult(offerWallIntent, OFFERWALL_REQUEST_CODE);
+        //Intent offerWallIntent = SponsorPayPublisher.getIntentForOfferWallActivity(cordova.getActivity().getApplicationContext(), true);
+        //cordova.getActivity().startActivityForResult(offerWallIntent, OFFERWALL_REQUEST_CODE);
+        OfferWallRequester.create(this).request(cordova.getActivity());
     }
     
     private PluginResult executeShowRewardedVideo(JSONObject options, CallbackContext callbackContext) {
@@ -103,6 +135,71 @@ public class FyberPlugin extends CordovaPlugin {
     }
 
     private void showRewardedVideo() {
-        //ssaPub.showRewardedVideo();
+        RewardedVideoRequester.create(this)
+                .withVirtualCurrencyRequester(getVirtualCurrencyRequester())
+                .request(cordova.getActivity());
+    }
+
+    private PluginResult executeShowInterstitial(JSONObject options, CallbackContext callbackContext) {
+        Log.w(LOGTAG, "executeShowInterstitial");
+        
+        this.showInterstitial( );
+        
+        callbackContext.success();
+
+        return null;
+    }
+
+    private void showInterstitial() {
+        InterstitialRequester.create(this)
+                .request(cordova.getActivity());
+    }
+
+    @Override
+    public void onAdAvailable(Intent intent) {
+        resetRequestingState();
+        
+        AdFormat adFormat = AdFormat.fromIntent(intent);
+        switch (adFormat) {
+            case OFFER_WALL:
+                //in our sample app, we want to show the offer wall in a single step.
+                cordova.getActivity().startActivityForResult(intent, OFFERWALL_REQUEST_CODE);
+                break;
+        }
+    }
+
+    @Override
+    public void onAdNotAvailable(AdFormat adFormat) {
+        FyberLogger.d(getLogTag(), "No ad available");
+    }
+
+    @Override
+    public void onRequestError(RequestError requestError) {
+        FyberLogger.d(getLogTag(), "Semething went wrong with the request: " + requestError.getDescription());
+    }
+
+    private VirtualCurrencyRequester getVirtualCurrencyRequester() {
+        return VirtualCurrencyRequester.create(this)
+                // user will get a toast notification upon reward
+                .notifyUserOnReward(true)
+//              this is the currency id for RV ad format
+//               you can refer to this -- http://developer.fyber.com/content/android/basics/rewarding-the-user/vcs/
+                .forCurrencyId("mondos")
+                ;
+    }
+
+    @Override
+    public void onError(VirtualCurrencyErrorResponse virtualCurrencyErrorResponse) {
+        FyberLogger.d(TAG, "VCS error received - " + virtualCurrencyErrorResponse.getErrorMessage());
+    }
+
+    @Override
+    public void onSuccess(VirtualCurrencyResponse virtualCurrencyResponse) {
+        FyberLogger.d(TAG, "VCS coins received - " + virtualCurrencyResponse.getDeltaOfCoins());
+    }
+
+    @Override
+    public void onRequestError(RequestError requestError) {
+        FyberLogger.d(TAG, "error requesting vcs: " + requestError.getDescription());
     }
 }
